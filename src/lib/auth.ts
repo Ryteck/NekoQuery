2ce928fc085b66784c75b927e172eca0b3e27c84
Ryan Gomes Martins <ryteckgm@gmail.com>
2025-05-30 11:14:24 -0300

    Add better-auth organization plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,10 @@ import { db } from "@/db";
 import * as schema from "@/db/schema/auth";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { openAPI } from "better-auth/plugins";
+import { openAPI, organization } from "better-auth/plugins";
 
 export const auth = betterAuth({
-	plugins: [openAPI()],
+	plugins: [openAPI(), organization()],
 
 	database: drizzleAdapter(db, {
 		provider: "pg",
--- src/repositories/project.ts
+++ src/repositories/project.ts
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import { user } from "@/db/schema/auth";
 import { participant } from "@/db/schema/participant";
 import {
 	type InsertProjectData,
@@ -12,10 +13,39 @@ export const listProjectsByUserId = async (
 ): Promise<ProjectData[]> =>
 	db.select().from(project).where(eq(project.userId, userId));
 
-export const showProjectById = async (id: string): Promise<ProjectData> => {
-	const selecteds = await db.select().from(project).where(eq(project.id, id));
-	const selected = selecteds[0];
-	return selected;
+export interface ShowProjectByIdReturn extends ProjectData {
+	members: Array<{
+		userId: string;
+		userName: string;
+		participantId: string;
+		participantRole: string;
+	}>;
+}
+
+export const showProjectById = async (
+	id: string,
+): Promise<ShowProjectByIdReturn> => {
+	const selectedProjects = await db
+		.select()
+		.from(project)
+		.where(eq(project.id, id));
+
+	const selectedProject = selectedProjects[0];
+
+	if (!selectedProject) throw new Error("Project not found");
+
+	const selectedMembers = await db
+		.select({
+			userId: user.id,
+			userName: user.name,
+			participantId: participant.id,
+			participantRole: participant.role,
+		})
+		.from(participant)
+		.innerJoin(user, eq(participant.userId, user.id))
+		.where(eq(participant.projectId, selectedProject.id));
+
+	return { ...selectedProject, members: selectedMembers };
 };
 
 export const createNewProject = async ({
